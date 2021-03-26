5134ed0bb2a339d7fac45d960ba1c191ed794a51
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-03-26 04:43:14 +0000

    Add benchmark criterion

--- benches/websocket_benchmark.rs
+++ benches/websocket_benchmark.rs
@@ -4,12 +4,20 @@ use binance::api::*;
 use binance::userstream::*;
 use binance::websockets::*;
 use std::sync::atomic::{AtomicBool, Ordering};
+use core::time::Duration;
 
-fn handle_msg(web_socket: &mut WebSockets<'_>) {
-    web_socket.public_handle_msg("test");
+fn handle_msg(web_socket: &mut WebSockets<'_>, body: &str) {
+    web_socket.public_handle_msg(&body);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("websockets-decoder");
+
+    let body = reqwest::blocking::get("https://api.binance.com/api/v3/ticker/price")
+        .unwrap().text().unwrap();
+
+    println!("Length: {}", body.len());
 
     let keep_running = AtomicBool::new(true); // Used to control the event loop
     let agg_trade: String = String::from("!ticker@arr");
@@ -17,12 +25,11 @@ fn criterion_benchmark(c: &mut Criterion) {
         Ok(())
     });
 
-    c.bench_function("handle_msg 20", |b| b.iter(|| handle_msg(&mut web_socket)));
+    group.sample_size(200);
+    group.measurement_time(Duration::new(35, 0));
+    group.bench_function("handle_msg 20", |b| b.iter(|| handle_msg(&mut web_socket, &body)));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-
-
-
-
