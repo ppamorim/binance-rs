d6854b688ce40093cf84b1f998cfefa58aa07504
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-03-30 04:28:27 +0100

    Speed up mapping

--- benches/websocket_benchmark.rs
+++ benches/websocket_benchmark.rs
@@ -14,20 +14,24 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("websockets-decoder");
 
-    let body = reqwest::blocking::get("https://api.binance.com/api/v3/ticker/price")
+    let all_symbols_json = reqwest::blocking::get("https://api.binance.com/api/v3/ticker/price")
         .unwrap().text().unwrap();
 
-    println!("Length: {}", body.len());
+    let btc_symbol_json = reqwest::blocking::get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+        .unwrap().text().unwrap();
 
-    let keep_running = AtomicBool::new(true); // Used to control the event loop
-    let agg_trade: String = String::from("!ticker@arr");
+    let mut web_socket_subscribed: WebSockets<'_> = WebSockets::new_with_subscription("!ticker@arr", |event: WebsocketEvent| {
+        Ok(())
+    });
+
     let mut web_socket: WebSockets<'_> = WebSockets::new(|event: WebsocketEvent| {
         Ok(())
     });
 
     group.sample_size(200);
     group.measurement_time(Duration::new(35, 0));
-    group.bench_function("handle_msg 20", |b| b.iter(|| handle_msg(&mut web_socket, &body)));
+    group.bench_function("handle_msg all symbols", |b| b.iter(|| handle_msg(&mut web_socket_subscribed, &all_symbols_json)));
+    group.bench_function("handle_msg BTCUSDT symbol", |b| b.iter(|| handle_msg(&mut web_socket, &btc_symbol_json)));
     group.finish();
 }
 
